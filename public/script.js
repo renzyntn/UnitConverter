15116e5eddb94003fe2297420ae3e4edf4f857ba
Renzy Antonio <renzy.antonio0112@gmail.com>
2025-05-31 21:22:40 +0800

    added script js for interactivity

---
+++ public/script.js
@@ -0,0 +1,7 @@
+const inputEl = document.querySelector("#input-el");
+const convertBtn = document.querySelector("#convert-el");
+const resetBtn = document.querySelector("#reset-el");
+const massEl = document.querySelector("#mass-el");
+const lengthEl = document.querySelector("#length-el");
+const volumeEl = document.querySelector("#volume-el");
+
